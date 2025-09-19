40f2dd391336df873f4b6ed34dadf5a6164a4e26
ragasmaryjoy <maryjoyragas1@gmail.com>
2025-09-19 21:28:51 +0800

    Add files via upload

---
+++ db_con.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";   
+$dbname = "enrollments"; 
+$username = "root";   
+$password = "";         
+
+try {
+    
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo " Connected successfully";
+} catch (PDOException $e) {
+    echo " Connection failed: " . $e->getMessage();
+}
+?>
---
+++ pdo.php
@@ -0,0 +1,12 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=enrollments_db;charset=utf8mb4';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo json_encode(['success' => false, 'message' => 'DB Error: ' . $e->getMessage()]);
+    exit;
+}
